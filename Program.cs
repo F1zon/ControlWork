26c4212c667e742fb60172e456c545e57023d5ff
Nikita Koroliov <nik_iv02@mail.ru>
2023-01-09 02:00:55 +0700

    add function (input)

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// main
+string[] words = new string[10];
+Input(words);
+
+// Функция ввода слов в массив 1
+string[] Input(string[] words) {
+    Console.WriteLine("Введите слова (максимум 10 слов, для остановки введите 'stop'):");
+    int i = 0;
+
+    for (; i < 10; i++) {
+        Console.Write($"[{i + 1}]: ");
+        words[i] = Console.ReadLine();
+        if (words[i] == "stop") break;
+    }
+
+    return words;
+}
+
